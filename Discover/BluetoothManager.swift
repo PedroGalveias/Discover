ec8460f11cf31cdcb1f9382b6fc0528a841ef2cc
Pedro Galveias <pedro.r.galveias@pm.me>
2024-06-19 18:07:11 +0200

    Init Proj

---
+++ Discover/BluetoothManager.swift
@@ -0,0 +1,8 @@
+//
+//  BluetoothManager.swift
+//  Discover
+//
+//  Created by Pedro Galveias on 06.05.2024.
+//
+
+import Foundation
---
+++ Discover/BluetoothStatusView.swift
@@ -0,0 +1,8 @@
+//
+//  BluetoothStatusView.swift
+//  Discover
+//
+//  Created by Pedro Galveias on 06.05.2024.
+//
+
+import Foundation
